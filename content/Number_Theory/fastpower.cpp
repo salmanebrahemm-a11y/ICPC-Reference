58e7a9aceca3821cb7a65fbd05380f6848010c6c
Salman Ebrahem <salman.ebrahem.m@gmail.com>
2026-08-11 16:35:24 +0300

    adding divisors

--- content/Number_Theory/fastpower.cpp
+++ content/Number_Theory/fastpower.cpp
@@ -1,14 +1,16 @@
- ll fast_pow(ll a, ll b) 
-  {
-    if (b==0) return 1;
-    if (b==1) return a;
-    ll ret=1;
-    if (b%2!=0) 
-    {
-       ret=a%mod;
-    }
-    ll x=fast_pow(a,b/2);
-    x=(x*x)%mod;
-    ret=(ret*x)%mod;
-    return ret%mod;
- }
+#include<bits/stdc++.h>
+using namespace std;
+
+int fast_pow(int a, int b, int mod) {
+   if (b==0) return 1;
+   if (b==1) return a;
+   int ret = 1;
+   if (b%2!=0) 
+   {
+      ret=a % mod;
+   }
+   int x = fast_pow(a, b / 2, mod);
+   x=(x*x) % mod;
+   ret=(ret*x)%mod;
+   return ret%mod;
+}
